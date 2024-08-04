53d5c95d6e41d3a1c835d32c0a6183814d465c1f
winuss <niceyss@gmail.com>
2024-08-04 15:35:47 +0900

    add enableCors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin:
+        process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:5173',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+  }
+
   await app.listen(3000);
 }
 bootstrap();
